1a3d6270adac63f2eb7daaf5e39e0fda3ff27c44
Shchff <grisaserbakov0@gmail.com>
2023-11-06 01:11:11 +0500

    Задача 1: Реализация архитектуры на основе MVC, команда создания проекта

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ import controller.Controller;
 import view.TelegramView;
 
 public class App {
+    // Задача 1
     public static void main(String[] args) {
         Controller controller = new Controller();
         TelegramView telegramView = new TelegramView(controller);
