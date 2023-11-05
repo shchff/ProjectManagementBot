2d8689c2a2c51ae37878ca82ae8e95fcac8b555d
Shchff <grisaserbakov0@gmail.com>
2023-11-06 00:22:51 +0500

    Добавил в Facade недостающие javadocs

--- src/main/java/model/Facade.java
+++ src/main/java/model/Facade.java
@@ -15,10 +15,19 @@ public abstract class Facade {
         // добавление в бд
         return new Response("Проект создан");
     }
+
+    /**
+     * Отправляет сообщение о выходе из бота
+     * @return
+     */
     static public Response exitBot() {
         return new Response("Работа бота завершена");
     }
 
+    /**
+     * Оптравляет сообщение с информацией о боте
+     * @return
+     */
     static public Response getHelp() {
         return new Response("Я - бот для работы с проектом. Вот список моих команд:\n " +
                 "/createProject - создание проекта \n " +
