85e9ee28f8da4a186f7be3569acbc9b442a951e8
Shchff <grisaserbakov0@gmail.com>
2023-11-06 01:11:11 +0500

    Задача 1: Реализация архитектуры на основе MVC, команда создания проекта

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ import controller.CLIController;
 import view.CLIView;
 
 public class App {
+    // Задача 1
     public static void main(String[] args) {
         CLIController controller = new CLIController();
         CLIView cliView = new CLIView(controller);
