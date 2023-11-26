7cc30445d8cab44d1157382c9b4e24b7ceb719a6
Shchff <grisaserbakov0@gmail.com>
2023-11-26 20:13:33 +0500

    Добавил .env

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,12 @@
 
 import controller.CLIController;
+import io.github.cdimascio.dotenv.Dotenv;
 import view.CLIView;
 
 public class App {
     public static void main(String[] args) {
-        CLIController controller = new CLIController();
-        CLIView cliView = new CLIView(controller);
-        cliView.startDialog();
+//        CLIController controller = new CLIController();
+//        CLIView cliView = new CLIView(controller);
+//        cliView.startDialog();
     }
 }
