c39c193685d428f27fcc31b61845cc97f459976d
Egor <zhora.klem@yandex.ru>
2023-11-27 00:23:50 +0500

    added StartCommand and user.model

---
+++ src/main/java/controller/command/StartCommand.java
@@ -0,0 +1,23 @@
+package controller.command;
+
+import controller.Facade;
+import view.response.Response;
+
+import java.util.ArrayList;
+/**
+ * Комманда запуска бота
+ */
+public class StartCommand extends Command{
+    /**
+     * @param command
+     * @param argsList
+     */
+    public StartCommand(Commands command, ArrayList<String> argsList) {
+        super(command, argsList);
+    }
+
+    @Override
+    public Response perform() {
+        return Facade.getStartMessage();
+    }
+}
---
+++ src/main/java/model/user/User.java
@@ -0,0 +1,42 @@
+package model.user;
+
+import model.project.Role;
+
+
+
+public class User {
+
+    private String username;
+    private Role role;
+    private Long userId;
+
+    public User(String username, Role role, Long userId) {
+        this.username = username;
+        this.role = role;
+        this.userId = userId;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
